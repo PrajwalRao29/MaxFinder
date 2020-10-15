113a6f37244dd1d3aed650ce1be64cea09fa4dfd
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-15 19:11:17 +0530

    Refactored the code to implement generic method

--- main/java/Max.java
+++ main/java/Max.java
@@ -22,12 +22,19 @@ public class Max {
 
     public String FindStringMax(String a,String b,String c)
     {
-       Integer g=a.length();
-        Integer h=b.length();
-        Integer i=c.length();
-        if((g.compareTo(FindIntMax(g,h,i))==0))
+        if (a.compareTo(b)>0 && a.compareTo(c)>0)
             return a;
-        else if ((h.compareTo(FindIntMax(g,h,i))==0))
+        else if (b.compareTo(a)>0 && b.compareTo(c)>0)
+            return b;
+        else
+            return c;
+    }
+
+    public <T extends Comparable<T>> T FindGenericMax(T a,T b,T c)
+    {
+        if (a.compareTo(b)>0 && a.compareTo(c)>0)
+            return a;
+        else if (b.compareTo(a)>0 && b.compareTo(c)>0)
             return b;
         else
             return c;
--- test/java/TestMax.java
+++ test/java/TestMax.java
@@ -4,19 +4,23 @@ import org.junit.Test;
 
 public class TestMax {
     Max m = new Max();
-    Integer a,b,c;
-    Float d,e,f;
-    String x,y,z;
+    Integer a, b, c;
+    Float d, e, f;
+    String x, y, z;
 
     @Before
-    public void initialize()
-    {
-        a=3;b=2;c=1;
-        d=3.00f;e=2.00f;f=1.00f;
-        x="Hi";
-        y="Wor";
-        z="Prajwal";
+    public void initialize() {
+        a = 3;
+        b = 2;
+        c = 1;
+        d = 3.00f;
+        e = 2.00f;
+        f = 1.00f;
+        x = "Hi";
+        y = "Wor";
+        z = "Prajwal";
     }
+
     @Test
     public void test1MaxatfirstInt() {
         Assert.assertSame(m.FindIntMax(a, b, c), a);
@@ -31,6 +35,7 @@ public class TestMax {
     public void test3MaxatThirdInt() {
         Assert.assertSame(m.FindIntMax(b, c, a), a);
     }
+
     @Test
     public void test4MaxatfirstFloat() {
         Assert.assertSame(m.FindFloatMax(d, e, f), d);
@@ -45,18 +50,34 @@ public class TestMax {
     public void test6MaxatThirdFloat() {
         Assert.assertSame(m.FindFloatMax(e, f, d), d);
     }
+
     @Test
     public void test7MaxatfirstString() {
-        Assert.assertSame(m.FindStringMax(z, x, y), z);
+        Assert.assertSame(m.FindStringMax(z, x, y), y);
     }
 
     @Test
     public void test8MaxatSecondString() {
-        Assert.assertSame(m.FindStringMax(x, z, y), z);
+        Assert.assertSame(m.FindStringMax(x, z, y), y);
     }
 
     @Test
     public void test9MaxatThirdString() {
-        Assert.assertSame(m.FindStringMax(x, y, z), z);
+        Assert.assertSame(m.FindStringMax(x, y, z), y);
+    }
+
+    @Test
+    public void test10MaxatSecondGeneric() {
+        Assert.assertSame(m.FindGenericMax(b, a, c), a);
+    }
+
+    @Test
+    public void test11MaxatfirstGeneric() {
+        Assert.assertSame(m.FindGenericMax(d, e, f), d);
+    }
+
+    @Test
+    public void test12MaxatThirdGeneric() {
+        Assert.assertSame(m.FindGenericMax(x, y, z), y);
     }
 }
