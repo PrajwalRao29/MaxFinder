13b090068dd614478b7a79ada11b18ceed8f7111
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-15 20:05:30 +0530

    Added functionality to print Maximum

--- main/java/GMax.java
+++ main/java/GMax.java
@@ -5,16 +5,23 @@ public class GMax<T>{
 
     public static <T extends Comparable<T>> T TestMaximum(T... elements)
     {   T max=elements[0];
-        for(T e:elements)
-        {
-            if(max.compareTo(e)<0)
-            {
-                max=e;
+        for(T e:elements) {
+            if (max.compareTo(e) < 0) {
+                max = e;
             }
         }
+        printMax(max,elements);
         return max;
     }
-
+    public static <T> void printMax(T max,T... elements)
+    {
+        System.out.println("The maximum among the parameters ");
+        for(T e:elements)
+        {
+            System.out.println(e+" ");
+        }
+        System.out.println("is "+ max );
+    }
     public static void main(String[] args) {
 
         GMax g=new GMax();
